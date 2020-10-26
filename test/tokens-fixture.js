e695cc3c69928c2e3b9f095e1ec2ea4526b79ef0
dillon <dillonkellar@protonmail.com>
2020-10-25 23:25:43 -0500

    Updated tokens fixture to ensure both cases of token ordering are covered

--- test/tokens-fixture.js
+++ test/tokens-fixture.js
@@ -1,3 +1,5 @@
+const { BigNumber } = require("ethers");
+
 let i = 0;
 
 const testTokensFixture = deployments.createFixture(async ({
@@ -6,11 +8,24 @@ const testTokensFixture = deployments.createFixture(async ({
   ethers
 }) => {
   const [signer] = await ethers.getSigners();
-  const MockERC20 = await ethers.getContractFactory('MockERC20');
-  const token0 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
-  const token1 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
-  let pair0, pair1;
   const weth = await ethers.getContract('weth', signer);
+  const MockERC20 = await ethers.getContractFactory('MockERC20');
+  let token0 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
+  let token1 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
+  let wethBN = BigNumber.from(weth.address);
+  let token0BN = BigNumber.from(token0.address);
+  let token1BN = BigNumber.from(token1.address);
+  // Coverage of case in observeTwoWayPrice where token is greater than weth
+  while (token0BN.gt(wethBN)) {
+    token0 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
+    token0BN = BigNumber.from(token0.address);
+  }
+  // Coverage of case in observeTwoWayPrice where token is greater than weth
+  while (wethBN.gt(token1BN)) {
+    token1 = await MockERC20.deploy(`Token${i++}`, `Token${i}`);
+    token1BN = BigNumber.from(token1.address);
+  }
+  let pair0, pair1;
   const uniswapFactory = await ethers.getContract('UniswapV2Factory', signer);
 
   await uniswapFactory.createPair(token0.address, weth.address).then(tx => tx.wait()).then(async ({ events }) => {
