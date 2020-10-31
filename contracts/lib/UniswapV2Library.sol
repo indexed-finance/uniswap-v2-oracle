9b3fdb6d63fecc685a9b057610370857a3866d4f
dillon <dillonkellar@protonmail.com>
2020-10-30 19:00:01 -0500

    changed >= to ^

--- contracts/lib/UniswapV2Library.sol
+++ contracts/lib/UniswapV2Library.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.6.0;
+pragma solidity ^0.6.0;
 
 /************************************************************************************************
 Originally from https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/libraries/UniswapV2Library.sol
